df72c8a00d37d216a3fa48e31c3a5be3db1fb283
joannes59 <joannes.landy@gmail.com>
2026-03-19 22:45:15 +0100

    multi model features

--- src/preprocess.py
+++ src/preprocess.py
@@ -16,16 +16,31 @@ import logging
 # Configuration de base : afficher les INFO dans la console
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    datefmt='%H:%M:%S'
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%H:%M:%S",
 )
 
 
-COLUMNS=["path", "filename", "width", "height",
-         "mode", "format", "size_bytes",  "bits", "label",
-         "is_grayscale", "property", "brightness", "contrast", "sharpness",
-          'dark_ratio', 'bright_ratio', 'median_intensity',
-          'property', 'state'
+COLUMNS = [
+    "path",
+    "filename",
+    "width",
+    "height",
+    "mode",
+    "format",
+    "size_bytes",
+    "bits",
+    "label",
+    "is_grayscale",
+    "property",
+    "brightness",
+    "contrast",
+    "sharpness",
+    "dark_ratio",
+    "bright_ratio",
+    "median_intensity",
+    "property",
+    "state",
 ]
 
 
@@ -34,16 +49,17 @@ class ImagePreprocess:
     Classe pour la gestion des images avec un dataframe sauvegardé.
     """
 
-    def __init__(self, 
-                 filepath: str = "data/images.parquet", 
-                 outpath: str = "preprocessed",
-                 datapath: str = "data"):
-        
+    def __init__(
+        self,
+        filepath: str = "data/images.parquet",
+        outpath: str = "preprocessed",
+        datapath: str = "data",
+    ):
+
         self.filepath = filepath
         self.datapath = datapath
         self.outpath = outpath
         self.df = pd.DataFrame()
-
 
     def load_dataframe(self) -> pd.DataFrame:
         """Charge le dataframe existant ou en crée un nouveau."""
@@ -55,7 +71,6 @@ class ImagePreprocess:
         else:
             self.df = pd.DataFrame(columns=COLUMNS)
 
-        
     def save(self):
         """Sauvegarde le dataframe au format parquet ou csv."""
         if self.filepath.endswith(".parquet"):
@@ -63,72 +78,67 @@ class ImagePreprocess:
         elif self.filepath.endswith(".csv"):
             self.df.to_csv(self.filepath, index=False)
 
-
-
     def update_label(self):
-        """ Update un dataframe avec les labels pour le traitement d'image """
+        """Update un dataframe avec les labels pour le traitement d'image"""
         start_time = time.time()
-                
+
         for index, row in self.df.iterrows():
-            
             if "cancer" in row["path"]:
                 label = "cancer"
             elif "normal" in row["path"]:
                 label = "normal"
             else:
                 label = "None"
-            
-            self.df.loc[index, ['label']] = [label]
-        
+
+            self.df.loc[index, ["label"]] = [label]
+
         end_time = round(time.time() - start_time, 3)
         logging.info(f" Labelized {self.df.shape[0]} images in {end_time} second")
-            
-        
+
     def preprocess_image(self, input_path, output_path):
-    
+
         # lecture image (RGB en BGR dans OpenCV)
         img = cv2.imread(input_path)
-    
+
         # conversion en grayscale
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
+
         # redimensionnement
         gray = cv2.resize(gray, (224, 224))
-    
+
         # conversion vers 3 canaux pour encodeur
         rgb = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
-    
+
         # sauvegarde
         cv2.imwrite(output_path, rgb)
-                
-    def preprocess(self):
-        """ Load path file image from dataframe and preprocess images """
+
+    def run(self):
+        """Load path file image from dataframe and preprocess images"""
         start_time = time.time()
         self.load_dataframe()
-        
+
         # select images to preprocess
         out_path = os.path.join(self.datapath, self.outpath)
         os.makedirs(out_path, exist_ok=True)
-        print('------outpath------', out_path)
-        
+
+        # création des répertoires par label
+        labels = self.df["label"].dropna().unique()
+        for label in labels:
+            label_path = os.path.join(out_path, str(label))
+            os.makedirs(label_path, exist_ok=True)
+
+        # Convertion des images
         for index, row in self.df.iterrows():
-            
-            if row['state'] == "None":
-                file_path_in = os.path.join(row["path"] , row["filename"])
-                file_path_out = os.path.join(out_path, row["filename"])
-                
+            if row["state"] == "None":
+                file_path_in = os.path.join(row["path"], row["filename"])
+                file_path_out = os.path.join(out_path, row["label"],  row["filename"])
+
                 self.preprocess_image(file_path_in, file_path_out)
-                
+
         end_time = round(time.time() - start_time, 3)
-        logging.info(f"Image convertion images in {end_time} second") 
-            
-        
-        
-        
+        logging.info(f"Image convertion images in {end_time} second")
+
 
 if __name__ == "__main__":
-    
     manager = ImagePreprocess()
-    manager.preprocess()
-    
-
+    manager.run()
